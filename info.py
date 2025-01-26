97da6fce71e3f11b89890b74c00599c8afeb3eea
Akash1040-nm <nr8821314@gmail.com>
2025-01-26 10:49:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -67,8 +67,8 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://nr8821314:CB5PODQ5hJ1dhdTu@cluster0.i2pzi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
-DATABASE_NAME = environ.get('DATABASE_NAME', "techvjclonefilterbot")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
+DATABASE_NAME = environ.get('DATABASE_NAME', "AkashTelegramMovieSearchbot")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'AkashCollection')
 
 # If Multiple Database Is True Then Fill All Three Below Database Uri Else You Will Get Error.
 O_DB_URI = environ.get('O_DB_URI', "")   # This Db Is For Other Data Store
