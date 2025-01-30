3275d9e8f0b94cb83f68f44414ca5aacfd2cce2f
Akash1040-nm <nr8821314@gmail.com>
2025-01-30 14:00:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -101,7 +101,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 't.me/RK_Movies8') # Support Chat Lin
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', False))
-PM_SEARCH = bool(environ.get('PM_SEARCH', True))
+PM_SEARCH = bool(environ.get('PM_SEARCH', False))
 BUTTON_MODE = bool(environ.get('BUTTON_MODE', False))
 MAX_BTN = bool(environ.get('MAX_BTN', True))
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
@@ -132,7 +132,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://modijiurl.com')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'modijiurl.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', '4ca5c91a78fc8c877a5ca3e79d7f473eb670e2c1')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/c/2290511828/3') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
